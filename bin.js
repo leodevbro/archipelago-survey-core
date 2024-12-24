839645d614906f688c5dc60b4de3fb500dbebf9f
Levan Katsadze <leodevbro@gmail.com>
2024-12-24 14:52:19 +0400

    To be usable with npx

---
+++ bin.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const mainGate = require("./dist/index");
+
+mainGate.readTest();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-export const aaa: number = 5;
-console.log(aaa);
+export const arpeSurveyCore = {
+  readTest: () => {
+    console.log("archipelago-survey-core (v0.0.2) ready to use.");
+  },
+
+  b: {
+    f: 4,
+    b: 9,
+    m: 7,
+  },
+} as const;
+
+export default arpeSurveyCore;
